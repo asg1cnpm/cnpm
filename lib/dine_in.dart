588ae7ede3231ae810088fe1dc352185dea0fbfd
cloudythy <65030707+cloudythy@users.noreply.github.com>
2021-11-06 04:11:37 +0700

    Update dine_in.dart

--- lib/dine_in.dart
+++ lib/dine_in.dart
@@ -7,6 +7,61 @@ class DineInButton extends StatelessWidget {
     Key? key,
   }) : super(key: key);
 
+  createPopup(BuildContext context) {
+    return showDialog(
+        context: context,
+        builder: (context) {
+          Future.delayed(Duration(seconds: 4), () {
+            Navigator.of(context).pop(true);
+          });
+          return Dialog(
+            child: Container(
+              width: 300,
+              height: 300,
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: ExactAssetImage('assets/icons/dine_in_popup.png'),
+                      fit: BoxFit.cover)),
+            ),
+          );
+        });
+  }
+
+  createAlertDialog(BuildContext context) {
+    TextEditingController tablenumber = TextEditingController();
+
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text("Insert your table number!"),
+            content: TextField(controller: tablenumber),
+            actions: <Widget>[
+              MaterialButton(
+                elevation: 5.0,
+                child: Text("Confirm Order"),
+                color: Colors.green[700],
+                textColor: Colors.white,
+                onPressed: () {
+                  createPopup(context);
+                  Future.delayed(Duration(seconds: 4), () {
+                    Navigator.of(context).pop(true);
+                  });
+                },
+              ),
+              MaterialButton(
+                  elevation: 5.0,
+                  child: Text("Cancel"),
+                  color: Colors.blueGrey,
+                  textColor: Colors.white,
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  })
+            ],
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Expanded(
@@ -28,7 +83,9 @@ class DineInButton extends StatelessWidget {
             ),
             Expanded(child: Container()),
             ElevatedButton(
-                onPressed: () {},
+                onPressed: () {
+                  createAlertDialog(context);
+                },
                 child: const Padding(
                   padding: EdgeInsets.symmetric(vertical: 10),
                   child: Text("Dine In"),
